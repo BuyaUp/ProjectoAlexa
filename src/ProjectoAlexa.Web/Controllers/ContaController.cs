7ef5d6f90a18b37f5c591d1a8f21ce07b889a87e
evander teixeira <evanderlord12@hotmail.com>
2020-10-18 00:49:59 +0100

    Front-End Update

---
+++ src/ProjectoAlexa.Web/Controllers/ContaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectoAlexa.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
