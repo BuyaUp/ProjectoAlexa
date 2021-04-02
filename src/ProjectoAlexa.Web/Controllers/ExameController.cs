e826c975f7ee3a25cfc594a6000e995c2796ab34
evander teixeira <evanderlord12@hotmail.com>
2021-04-02 14:13:10 +0100

    update - 02/04/2020 - Exibir questionário no exame

--- src/ProjectoAlexa.Web/Controllers/ExameController.cs
+++ src/ProjectoAlexa.Web/Controllers/ExameController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjectoAlexa.Data.Repositorios;
+using ProjectoAlexa.Data.Repositorios.Questionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,27 @@ namespace ProjectoAlexa.Web.Controllers
 {
     public class ExameController : Controller
     {
+
         // GET: Exame
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult FazerExame()
+        public ActionResult FazerExame(string IdCandidatura)
         {
-            return View();
+            var candidatura = CandidaturaRepositorio.BuscarPeloId(IdCandidatura);
+            var questionario = QuestionarioRepositorio.BuscarTodos().Where(q => q.AreaCandidaturaId == candidatura.AreaCandidaturaId).FirstOrDefault();
+
+            if (questionario == null)
+            {
+                ViewBag.Mensagem = "Erro ao realizar exame, verifique o estado da sua candidatura!";
+
+                return View();
+            }
+
+            return View(questionario);
+           
         }
     }
 }
