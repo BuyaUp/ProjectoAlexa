0f3e02403261319c407871376940907b6a29f203
evander teixeira <evanderlord12@hotmail.com>
2021-04-25 09:26:00 +0100

    Update 25/04/20201 - Fazer Exame

--- src/ProjectoAlexa.Web/Scripts/ScriptsDoProjecto/Exame.js
+++ src/ProjectoAlexa.Web/Scripts/ScriptsDoProjecto/Exame.js
@@ -1,3 +1,27 @@
-﻿function  TerminarExame(){
+﻿var listaResposta = [];
+
+function terminarExame() {
+    //Verificar cada radio button
+    $("input:radio").each(function (index) {
+        //Verificar apenas as radios marcadas
+        if ($(this).prop("checked")) {
+            alert(index + ": IdResposta= " + $(this).attr("idResposta") + " : IdPergunta= " + $(this).attr("idPergunta") + " resposta: " + $(this).attr("texto"));
+
+
+            //Adicionar respostas na lista de respostas
+            var resposta = {
+                PerguntaId =$(this).attr("idPergunta"),
+                RespostaId =$(this).attr("idResposta")
+            }
+
+            //Enviar os dados e cadastrar na tabela prova
+
+        }
+    });
 
 }
+
+
+
+
+
